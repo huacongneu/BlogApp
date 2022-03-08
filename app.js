e302ad261873122c38615448f7085be73bf34e28
huacong <cong.h@northeastern.edu>
2022-03-07 22:50:32 -0800

    Update Home Page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Dear Diary, I...";
+const homeStartingContent = "This is an online diary service, providing personal diaries and journals - it's 100% free! Writing a daily record of your life is a good way to make sure your memories and experiences stay alive. It lets you keep track and reflect on your past and learn from your mistakes. It can also be tremendously therapeutic. Not only to record fun and adventurous moments, but also sad and scary times. It can be helpful to be able to document changes in your life in an online journal. Go ahead and post your own diary today.";
 const aboutContent = "This is a 100% Free online diary. It can also be used as a blog if you want :)";
 const contactContent = "Please feel free to contact me if you have any questions or concerns.";
 
